f8ab54ae622c8fe90dee84f45a1e4f741793a06d
Nikita <ndmatyash@gmail.com>
2021-05-19 22:57:17 +0300

    Inserting to db

--- app/src/main/java/com/mirea/matyash/mireaproject/MainActivity.java
+++ app/src/main/java/com/mirea/matyash/mireaproject/MainActivity.java
@@ -33,16 +33,14 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                StoryDialog ds = new StoryDialog();
+                ds.show(getSupportFragmentManager(), "mirea");
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_browser, R.id.nav_clalculator, R.id.simplePlayer, R.id.sensors, R.id.camera, R.id.audioRecorder, R.id.settings)
+                R.id.nav_home, R.id.nav_browser, R.id.nav_clalculator, R.id.simplePlayer, R.id.sensors, R.id.camera, R.id.audioRecorder, R.id.settings, R.id.stories)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -52,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
---
+++ app/src/main/java/com/mirea/matyash/mireaproject/stories.java
@@ -0,0 +1,28 @@
+package com.mirea.matyash.mireaproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class stories extends Fragment {
+
+    public stories() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_stories, container, false);
+        return view;
+    }
+}
